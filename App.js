99f1161ce7943cc886ca5967934c02f2ff5e8918
lluisguardiola <lluisguardiola.h@gmail.com>
2020-09-29 14:47:39 -0500

    flatlist rendering todoItem component

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React, { useState } from 'react'
 import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native'
 import Header from './components/header'
+import TodoItem from './components/todoitem'
 
 export default function App() {
 	const [todos, setTodos] = useState([
 		{text: 'Get groceries', key: '1'},
 		{text: 'learn Expo', key: '2'}
 	])
+
+	const pressHandler = (key) => {
+		setTodos((prevTodos) => {
+			return prevTodos.filter(todo => todo.key !== key)
+		})
+	}
 
 	return (
 		<View style={styles.container}>
@@ -18,7 +25,10 @@ export default function App() {
 						// keyExtractor={()}
 						data={todos}
 						renderItem={({item}) => (
-							<Text>{item.text}</Text>
+							<TodoItem 
+								item={item}
+								pressHandler={pressHandler}
+							/>
 						)}
 					/>
 				</View>
